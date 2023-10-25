ad35344ca886ae95209bc3b230de6c4ccf259541
Luis David <luisdavid777777@gmail.com>
2023-10-24 19:59:36 -0500

    Added pistol

---
+++ Assets/Door/Door.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+
+    [SerializeField] private Camera mainCamera;
+
+
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+
+
+    private void Update()
+    {
+        
+    }
+
+
+
+
+
+}
