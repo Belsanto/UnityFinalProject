d6503f0793da2656c0a0117da55b2ed496b509c5
Santiago Velandia Gallo <santiago.velandiag@uqvirtual.edu.co>
2023-10-27 05:27:42 -0500

    End 1st sprint

--- Assets/CanvasPrefabs/GameOverMenuController.cs
+++ Assets/CanvasPrefabs/GameOverMenuController.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverMenuController : MonoBehaviour
 {
+    [SerializeField] private GameObject win;
+    [SerializeField] private GameObject die;
     public void TryAgain()
     {
         Time.timeScale = 1; // Resume the game time
@@ -17,5 +20,11 @@ public class GameOverMenuController : MonoBehaviour
         Time.timeScale = 1; // Make sure the game is not paused
         int buildIndex = SceneManager.GetActiveScene().buildIndex - 1;
         SceneManager.LoadScene(buildIndex); // Replace '0' with the build index of your main menu scene
+    }
+
+    public void SetLoseScreen()
+    {
+        win.SetActive(false);
+        die.SetActive(true);
     }
 }
